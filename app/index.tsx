dd1025a725786fab766060f369d9596969d2ac9b
Andrea Acosta <acostaandreadev@gmail.com>
2025-04-15 09:39:24 -0300

    chore: update dependencies and add Tailwind CSS configuration
    - Updated expo from 52.0.43 to 52.0.46
    - Updated expo-splash-screen from 0.29.22 to 0.29.23
    - Updated react-native-reanimated from 3.16.1 to 3.16.2
    - Added nativewind and tailwindcss as dependencies
    - Created tailwind.config.js for Tailwind CSS configuration
    - Added nativewind-env.d.ts to TypeScript configuration

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className='mt-1'>
+      <Text className='text-3xl font-bold'>app</Text>
+    </View>
+  )
+}
+
+export default App
